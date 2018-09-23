93647bf3862063a62a13d43d0d53bb3443cf397c
Valerie Rojdestvenskaia <vrojdestvenskaia@gmail.com>
2018-09-23 18:07:24 -0400

    updated

--- config/website.js
+++ config/website.js
@@ -7,7 +7,7 @@ module.exports = {
   siteTitleAlt: 'Valerie', // Alternative Site title for SEO
   siteUrl: 'valerierojdes.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logos/logo-48.png', // Used for SEO and manifest
+  // siteLogo: '/logos/logo-48.png', // Used for SEO and manifest
   siteDescription: `Valerie's Portfolio`,
 
   siteFBAppID: '123456789', // Facebook App ID
