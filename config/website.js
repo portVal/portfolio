97246226ccedfd5aea875b727090509357c6cdde
StefanPogatchev <44100349+StefanPogatchev@users.noreply.github.com>
2018-10-12 15:40:31 -0400

    Update website.js

--- config/website.js
+++ config/website.js
@@ -4,10 +4,12 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
   siteTitle: `Stefan's Portfolio`, // Navigation and Site Title
-  siteTitleAlt: 'Valerie', // Alternative Site title for SEO
+
   siteUrl: 'stefan.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   // siteLogo: '/logos/logo-48.png', // Used for SEO and manifest
+  siteDescription: `Stefan's Portfolio`,
+   // siteLogo: '/logos/logo-48.png', // Used for SEO and manifest
   siteDescription: `Stefan's Portfolio`,
 
   siteFBAppID: '123456789', // Facebook App ID
