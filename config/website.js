e9a4a0e9f243463e8b8224182330690fe63b8d4a
StefanPogatchev <44100349+StefanPogatchev@users.noreply.github.com>
2018-10-12 15:38:38 -0400

    added name change

--- config/website.js
+++ config/website.js
@@ -3,16 +3,16 @@ const tailwind = require('../tailwind');
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: `Valerie's Portfolio`, // Navigation and Site Title
+  siteTitle: `Stefan's Portfolio`, // Navigation and Site Title
   siteTitleAlt: 'Valerie', // Alternative Site title for SEO
-  siteUrl: 'valerierojdes.com', // Domain of your site. No trailing slash!
+  siteUrl: 'stefan.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   // siteLogo: '/logos/logo-48.png', // Used for SEO and manifest
   siteDescription: `Stefan's Portfolio`,
 
   siteFBAppID: '123456789', // Facebook App ID
   userTwitter: '@stefan', // Twitter Username
-  ogSiteName: 'valerie', // Facebook Site Name
+  ogSiteName: 'stefan', // Facebook Site Name
   ogLanguage: 'de_DE', // Facebook Language
 
   // Manifest and Progress color
